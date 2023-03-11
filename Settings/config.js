a8a6c60c9131de1286f2549f03283eb38d2414ed
Kaje INC <97894113+k6je@users.noreply.github.com>
2023-03-11 12:16:05 +0300

    Add files via upload

---
+++ Settings/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    tokens: [
+        "ODc2NTI2ODkzNDM5MDIxMDc2.YRlXSw.3CcGk4HOhzTLv_DMM5ojjvEcPfY"
+    ],//Tokenlerinizi buraya girin
+
+    result: 0, //Elleme
+    maxUsers: 5, //Token basina MassDM atilacak kisi sayisi
+
+    message: {
+
+        content: `MESAJINI BURAYA GIR`, //Mass DM Mesaji
+
+    }
+
+}
